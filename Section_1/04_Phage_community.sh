53d398869b83e0e360d06ba3759ce75d0ad27eea
Qifeng-Zhang <wofanzake@outlook.com>
2024-08-29 21:42:54 +0800

    Update 04_Phage_community.sh

--- Section_1/04_Phage_community.sh
+++ Section_1/04_Phage_community.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+#########################################################################
+# Description: Analysis of phage communities using virsorter2, virbrant, checkv, cd-hit, and coverm.
+# Requires: virsorter v2.2.4, vibrant v1.2.1, checkv v1.0.1, cd-hit v4.8.1, coverm v0.6.1 and contig (*.fa files).
+#########################################################################
+
 
