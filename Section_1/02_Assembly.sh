db9663caef71412ded8eaa925c02ed6cc33dc26f
Qifeng-Zhang <wofanzake@outlook.com>
2024-08-29 20:49:47 +0800

    Update 02_Assembly.sh

--- Section_1/02_Assembly.sh
+++ Section_1/02_Assembly.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+#########################################################################
+# Description: Quality control of metagenomic raw reads.
+# Requires: Kneaddata v0.12.0 and metagenomic raw reads (*.fastq files).
+#########################################################################
+
+for file in `ls /PATH_TO_CLEAN_READS/*.trimmed.1.fastq`
+do
+j=`basename $file .trimmed.1.fastq`
+megahit  \
+-1 /PATH_TO_CLEAN_READS/"$j".trimmed.1.fastq \
+-2 /PATH_TO_CLEAN_READS/"$j".trimmed.2.fastq \
+-o /PATH_TO_OUTPUT/$j \
+--k-list 21,41,61,81,99 \
+--min-contig-len 1000 \
+--out-prefix $j \
+done
+
+#########################################################################
