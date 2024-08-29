4954d697c0f8088a9d4ace14b2af75de21aa95e8
Qifeng-Zhang <wofanzake@outlook.com>
2024-08-29 20:41:45 +0800

    Update 01_Quality_control.sh

--- Section_1/01_Quality_control.sh
+++ Section_1/01_Quality_control.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+# Description: Quality control of metagenomic raw reads.
+# Requires: Kneaddata v0.12.0 and metagenomic raw reads (*.fastq files).
+
+for file in `ls /PATH_TO_FASTQ_FILES/*1.fastq`
+do
+j=`basename $file _1.fastq`
+kneaddata \
+-i1 /PATH_TO_FASTQ_FILES/${j}_1.fastq \
+-i2 /PATH_TO_FASTQ_FILES/${j}_2.fastq \
+-o /PATH_TO_OUTPUT/ \
+--bypass-trf \
+--output-prefix $j \
+--trimmomatic /PATH_TO_TRIMMOMATIC/ \
+--remove-intermediate-output
+done
