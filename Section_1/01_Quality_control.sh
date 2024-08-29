028acf9f454d8a93da3555b39d7752e2de4c2c32
Qifeng-Zhang <wofanzake@outlook.com>
2024-08-29 20:51:56 +0800

    Update 01_Quality_control.sh

--- Section_1/01_Quality_control.sh
+++ Section_1/01_Quality_control.sh
@@ -5,13 +5,13 @@
 # Requires: Kneaddata v0.12.0 and metagenomic raw reads (*.fastq files).
 #########################################################################
 
-for file in `ls /PATH_TO_FASTQ_FILES/*1.fastq`
+for file in `ls /PATH_TO_RAW_READS/*1.fastq`
 do
 j=`basename $file _1.fastq`
 kneaddata \
--i1 /PATH_TO_FASTQ_FILES/${j}_1.fastq \
--i2 /PATH_TO_FASTQ_FILES/${j}_2.fastq \
--o /PATH_TO_OUTPUT/ \
+-i1 /PATH_TO_RAW_READS/${j}_1.fastq \
+-i2 /PATH_TO_RAW_READS/${j}_2.fastq \
+-o /PATH_TO_OUTPUT_CLEAN_READS/ \
 --bypass-trf \
 --output-prefix $j \
 --trimmomatic /PATH_TO_TRIMMOMATIC/ \
