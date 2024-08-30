bf657f779a0a54ff02b34e50d702f4970e6b568b
Qifeng-Zhang <wofanzake@outlook.com>
2024-08-30 10:20:30 +0800

    Update 04_Phage_community.sh

--- Section_1/04_Phage_community.sh
+++ Section_1/04_Phage_community.sh
@@ -2,7 +2,34 @@
 
 #########################################################################
 # Description: Analysis of phage communities using virsorter2, virbrant, checkv, cd-hit, and coverm.
-# Requires: virsorter v2.2.4, vibrant v1.2.1, checkv v1.0.1, cd-hit v4.8.1, coverm v0.6.1 and contig (*.fa files).
+# Requires: virsorter v2.2.4, virbrant v1.2.1, checkv v1.0.1, cd-hit v4.8.1, coverm v0.6.1 and contig files (*.fa files).
 #########################################################################
 
+for file in `ls /PATH_TO_CONTIG_FILES/*.contigs.fa`
+do
+j=`basename $file .contigs.fa`
+virsorter run \
+-w /PATH_TO_OUTPUT_VS2/$j \
+-i /PATH_TO_CONTIG_FILES/$j.contigs.fa \
+--min-length 1500 \
+--rm-tmpdir \
+--verbose \
+--prep-for-dramv
+done
+
+#########################################################################
+
+for file in `ls /PATH_TO_CONTIG_FILES/*.contigs.fa`
+do
+j=`basename $file .contigs.fa`
+VIBRANT_run.py \
+-i /PATH_TO_CONTIG_FILES/$j.contigs.fa \
+-f nucl \
+-l 5000 \
+-no_plot \
+-folder /PATH_TO_OUTPUT_VB/$j \
+-d /PATH_TO_VB_DATABASE/
+done
+
+#########################################################################
 
