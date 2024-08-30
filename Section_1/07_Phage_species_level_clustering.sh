6f0fc9583e19cf68d76790e026802de60969718a
Qifeng-Zhang <wofanzake@outlook.com>
2024-08-30 11:40:16 +0800

    Update 07_Phage_species_level_clustering.sh

--- Section_1/07_Phage_species_level_clustering.sh
+++ Section_1/07_Phage_species_level_clustering.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+#########################################################################
+# Description: Species-level clustering of phage sequences using cd-hit.
+# Requires: cd-hit v2.3.1 and phage sequnences merge file (vseq_before_cv_merge.fa).
+#########################################################################
