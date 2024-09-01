8f92c04241b6c8c0d03db48979e899642848f407
Qifeng-Zhang <wofanzake@outlook.com>
2024-09-01 15:57:55 +0800

    Update 09_Time_decay_relationship.R

--- Section_1/09_Time_decay_relationship.R
+++ Section_1/09_Time_decay_relationship.R
@@ -1,7 +1,8 @@
+#########################################################################
 
 library(vegan)
 
-spe <- read.delim('virus-abundance-jxz-b-cv.txt', sep = '\t', row.names = 1, check.names = FALSE)
+spe <- read.delim('bacterial-abundance.txt', sep = '\t', row.names = 1, check.names = FALSE)
 spe <- data.frame(t(spe))
 
 comm_sim <- 1 - as.matrix(vegan::vegdist(spe, method = 'bray'))
@@ -12,4 +13,23 @@ comm_sim <- reshape2::melt(comm_sim)
 comm_sim <- subset(comm_sim, value != 0)
 head(comm_sim)
 
-write.table(comm_sim, 'comm_sim.vOTU.JXZ.txt', sep = '\t', row.names = FALSE, quote = FALSE)
+write.table(comm_sim, 'bacterial_time_decay.txt', sep = '\t', row.names = FALSE, quote = FALSE)
+
+#########################################################################
+
+library(vegan)
+
+spe <- read.delim('phage-abundance.txt', sep = '\t', row.names = 1, check.names = FALSE)
+spe <- data.frame(t(spe))
+
+comm_sim <- 1 - as.matrix(vegan::vegdist(spe, method = 'bray'))
+
+diag(comm_sim) <- 0
+comm_sim[upper.tri(comm_sim)] <- 0
+comm_sim <- reshape2::melt(comm_sim)
+comm_sim <- subset(comm_sim, value != 0)
+head(comm_sim)
+
+write.table(comm_sim, 'phage_time_decay.txt', sep = '\t', row.names = FALSE, quote = FALSE)
+
+#########################################################################
