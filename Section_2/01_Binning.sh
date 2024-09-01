d44b3a97263d41234b469095ca658d45b5dd5374
Qifeng-Zhang <wofanzake@outlook.com>
2024-09-01 16:31:53 +0800

    Update 01_Binning.sh

--- Section_2/01_Binning.sh
+++ Section_2/01_Binning.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
 
+#########################################################################
+# Description: Binning metagenomic data using metawrap.
+# Requires: metawrap v1.3.2, clean reads (*.fastq), and contig files (*.contigs.fa).
+#########################################################################
+
+for file in `ls /PATH_TO_CLEAN_READS/*_1.fastq`
+do
+basename=`basename $file _1.fastq`
+metawrap binning \
+-o /PATH_TO_OUTPUT/${basename}_binning \
+--metabat2 \
+--maxbin2 \
+--concoct \
+-a /PATH_TO_CONTIG/${basename}.contigs.fa \
+/PATH_TO_CLEAN_READS/${basename}_1.fastq \
+/PATH_TO_CLEAN_READS/${basename}_2.fastq
+done
+
+#########################################################################
