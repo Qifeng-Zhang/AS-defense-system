9896991f75d56d466f2721949604a029d6056700
Qifeng-Zhang <wofanzake@outlook.com>
2024-09-02 09:19:21 +0800

    Update 02_Gene_prediction_contig.sh

--- Section_2/02_Gene_prediction_contig.sh
+++ Section_2/02_Gene_prediction_contig.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+#########################################################################
+# Description: Use prodigal to predict orfs in contigs.
+# Requires: prodigal v2.6.3 and contig files (*.contigs.fa).
+#########################################################################
+
+for file in `ls /PATH_TO_CONTIG_FILES/*.contigs.fa`
+do
+j=`basename $file .contigs.fa`
+prodigal \
+-i /PATH_TO_CONTIG_FILES/$j.contigs.fa \
+-o /PATH_TO_OUTPUT_ORF_FILES/$j.gff \
+-a /PATH_TO_OUTPUT_PROTEIN_FILES/$j.protein.fa \
+-d /PATH_TO_OUTPUT_DNA_FILES/$j.DNA.fa \
+-f gff -p meta
+done
+
+#########################################################################
