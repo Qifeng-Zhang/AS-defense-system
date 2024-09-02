81f9b55b003332d83099bb01f4efd0d3bc78513c
Qifeng-Zhang <wofanzake@outlook.com>
2024-09-02 10:40:37 +0800

    Update 10_Phylogenetic_tree_MAG.sh

--- Section_2/10_Phylogenetic_tree_MAG.sh
+++ Section_2/10_Phylogenetic_tree_MAG.sh
@@ -1,1 +1,18 @@
+#!/bin/bash
 
+#########################################################################
+# Description: Using phylophlan to construct a phylogenetic tree of MAGs.
+# Requires: phylophlan v3.1.1 and MAG files (*.MAG.fa).
+#########################################################################
+
+phylophlan \
+-i /PATH_TO_MAG_FILES/*.MAG.fa \
+--output_folder /PATH_TO_OUTPUT_TREE/ \
+-d phylophlan \
+--databases_folder /PATH_TO_PHYLOPHLAN_DATABASE/ \
+-f /PATH_TO_CFG/supermatrix_aa.cfg \
+--genome_extension .fa \
+--diversity high \
+--verbose
+
+#########################################################################
