344f380cc8860b1a59afca111a2b2eb030c992c3
Qifeng-Zhang <wofanzake@outlook.com>
2024-09-02 11:10:26 +0800

    Update 02_CRISPR_spacer_de-redundancy.sh

--- Section_3/02_CRISPR_spacer_de-redundancy.sh
+++ Section_3/02_CRISPR_spacer_de-redundancy.sh
@@ -14,4 +14,20 @@ cd-hit-est \
 -c 1.0 -G 0 -aS 1.0 -d 0 -g 1 -M 0
 done
 
+for file in `ls /PATH_TO_CRASS_FILES/Spacers_dr/*.Spacers_dr.fasta`
+do
+echo "Processing $file..."
+j=`basename $file .Spacers_dr.fasta`
+sed -i "s/>/>$j\_/" /PATH_TO_CRASS_FILES/Spacers_dr/$j.Spacers_dr.fasta
+done
+
+#
+cat /PATH_TO_CRASS_FILES/Spacers_dr/*.Spacers_dr.fasta > /PATH_TO_CRASS_FILES/Spacers_dr/Spacers_total.fasta
+
+#
+cd-hit-est \
+-i /PATH_TO_CRASS_FILES/Spacers_dr/Spacers_total.fasta \
+-o /PATH_TO_CRASS_FILES/Spacers_dr/Spacers_total_dr.fasta \
+-c 1.0 -G 0 -aS 1.0 -d 0 -g 1 -M 0
+
 #########################################################################
