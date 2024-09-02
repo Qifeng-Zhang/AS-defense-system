086703705eb77c472dd3cdff0db7460aa9b78e64
Qifeng-Zhang <wofanzake@outlook.com>
2024-09-02 10:36:17 +0800

    Update 09_ANI_MAG.sh

--- Section_2/09_ANI_MAG.sh
+++ Section_2/09_ANI_MAG.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+#########################################################################
+# Description: Use fastANI to calculate ANI between MAGs.
+# Requires: fastANI v1.34 and MAG files (*.MAG.fa).
+#########################################################################
+
+readlink -f /PATH_TO_MAG_FILES/*.MAG.fa > MAG_path.txt
+
+fastANI --ql MAG_path.txt --rl MAG_path.txt -o MAG_ANI.txt
+
+#########################################################################
