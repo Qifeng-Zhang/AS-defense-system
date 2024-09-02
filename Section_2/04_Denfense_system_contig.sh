d8cfb0c76c42f52aa171e5a83d8f6d13ce315816
Qifeng-Zhang <wofanzake@outlook.com>
2024-09-02 09:30:43 +0800

    Update 04_Denfense_system_contig.sh

--- Section_2/04_Denfense_system_contig.sh
+++ Section_2/04_Denfense_system_contig.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+#########################################################################
+# Description: Identify complete defense systems in a contig using DefenseFinder.
+# Requires: defensefinder v1.2.1 and contig protein files (*.protein.fa).
+#########################################################################
+
+for file in `ls /PATH_TO_CONTIG_PROTEIN_FILES/*.protein.fa`
+do
+j=`basename $file .protein.fa`
+defense-finder run \
+-o /PATH_TO_OUTPUT_DS/$j \
+--db-type gembase \
+/PATH_TO_CONTIG_PROTEIN_FILES/$j.protein.fa
+done
+
+#########################################################################
