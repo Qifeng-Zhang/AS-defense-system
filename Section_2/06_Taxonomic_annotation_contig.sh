0e6d896988400967d97f480b4196ebd84bac9112
Qifeng-Zhang <wofanzake@outlook.com>
2024-09-02 09:50:10 +0800

    Update and rename 06_Taxonomy_annotation_contig.sh to 06_Taxonomic_annotation_contig.sh

---
+++ Section_2/06_Taxonomic_annotation_contig.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#########################################################################
+# Description: Taxonomic annotation of contigs containing complete defense systems using kraken2 and krakentools.
+# Requires: Kraken2 v2.1.3, krakentools, and defense system contig files (*.dfcontig.fasta).
+#########################################################################
+
+for file in `ls /PATH_TO_DS_CONTIG_FILES/*.dfcontig.fasta`
+do
+j=`basename $file .dfcontig.fasta`
+kraken2 \
+--db /PATH_TO_KRAKEN2_DATABASE/ \
+--output /PATH_TO_OUTPUT_KRAKEN/$j.kraken \
+--report /PATH_TO_OUTPUT_KREPORT/$j.kreport \
+/PATH_TO_DS_CONTIG_FILES/$j.dfcontig.fasta
+done
+
+#########################################################################
+
+for file in `ls /PATH_TO_OUTPUT_KREPORT/$j.kreport`
+do
+j=`basename $file .kreport`
+echo "Processing $j..."
+python3 /home/zqf/KrakenTools/kraken2_translate.py \
+--report /PATH_TO_OUTPUT_KREPORT/$j.kreport \
+--classification /PATH_TO_OUTPUT_KRAKEN/$j.kraken \
+--output /PATH_TO_OUTPUT_TAXONOMY/$j.csv \
+--mpa-format
+done
+
+#########################################################################
--- Section_2/06_Taxonomy_annotation_contig.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#########################################################################
-# Description: Taxonomic annotation of contigs containing complete defense systems using kraken2 and bracken.
-# Requires: defensefinder v1.2.1 and MAG protein files (*.protein.fa).
-#########################################################################
