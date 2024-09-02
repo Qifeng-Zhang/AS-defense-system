b63c68bc8c9c8fae187f1381c6f1599416bf7386
Qifeng-Zhang <wofanzake@outlook.com>
2024-09-02 10:53:18 +0800

    Update 02_CRISPR_spacer_de-redundancy.sh

--- Section_3/02_CRISPR_spacer_de-redundancy.sh
+++ Section_3/02_CRISPR_spacer_de-redundancy.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+#########################################################################
+# Description: De-redundancy of CRISPR spacers using CD-HIT.
+# Requires: cd-hit v2.3.1 and CRISPR spacer files (*.Spacers.fasta).
+#########################################################################
+
+for file in `ls /PATH_TO_CRASS_FILES/Spacers/*.Spacers.fasta`
+do
+j=`basename $file .Spacers.fasta`
+cd-hit-est \
+-i /PATH_TO_CRASS_FILES/Spacers/$j.Spacers.fasta \
+-o /PATH_TO_CRASS_FILES/Spacers_dr/$j.Spacers_dr.fasta \
+-c 1.0 -G 0 -aS 1.0 -d 0 -g 1 -M 0
+done
+
+#########################################################################
