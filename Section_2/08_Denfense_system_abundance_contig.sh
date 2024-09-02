bb08b2cc70f14f58eefdaedb7d05cd3b356d9d6b
Qifeng-Zhang <wofanzake@outlook.com>
2024-09-02 10:14:36 +0800

    Update 08_Denfense_system_abundance_contig.sh

--- Section_2/08_Denfense_system_abundance_contig.sh
+++ Section_2/08_Denfense_system_abundance_contig.sh
@@ -1,1 +1,6 @@
+#!/bin/bash
 
+#########################################################################
+# Description: Calculate the abundance of defense systems using coverm.
+# Requires: coverm v0.6.1, seqkit v2.3.1, and de-redundancy high confidence phage sequnences merge file (vseq_cv_merge_high_confidence_5k_dr.fa).
+#########################################################################
