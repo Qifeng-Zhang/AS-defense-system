236d3f75fb64977e0c209cb576c3947d5826a1fe
Qifeng-Zhang <wofanzake@outlook.com>
2024-09-02 10:36:25 +0800

    Update 08_Denfense_system_abundance_contig.sh

--- Section_2/08_Denfense_system_abundance_contig.sh
+++ Section_2/08_Denfense_system_abundance_contig.sh
@@ -2,7 +2,7 @@
 
 #########################################################################
 # Description: Calculate the abundance of defense systems using coverm.
-# Requires: coverm v0.6.1, seqkit v2.3.1, and and defense system contig files (*.dfcontig.fasta).
+# Requires: coverm v0.6.1, seqkit v2.3.1, and defense system contig files (*.dfcontig.fasta).
 #########################################################################
 
 for file in `ls /PATH_TO_CLEAN_READS/*_1.fq.gz`
