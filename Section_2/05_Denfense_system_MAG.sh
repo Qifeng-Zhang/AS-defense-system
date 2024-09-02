e9785b196e424e2269489bb629088a35b76ec67a
Qifeng-Zhang <wofanzake@outlook.com>
2024-09-02 09:31:39 +0800

    Update 05_Denfense_system_MAG.sh

--- Section_2/05_Denfense_system_MAG.sh
+++ Section_2/05_Denfense_system_MAG.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
 
+#########################################################################
+# Description: Identify complete defense systems in MAGs using DefenseFinder.
+# Requires: defensefinder v1.2.1 and MAG protein files (*.protein.fa).
+#########################################################################
+
+for file in `ls /PATH_TO_MAG_PROTEIN_FILES/*.protein.fa`
+do
+j=`basename $file .protein.fa`
+defense-finder run \
+-o /PATH_TO_OUTPUT_DS/$j \
+--db-type gembase \
+/PATH_TO_MAG_PROTEIN_FILES/$j.protein.fa
+done
+
+#########################################################################
